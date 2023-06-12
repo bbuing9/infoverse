052fa9e4b448327f95b256bad544d4d1313dfac5
jaehyungkim <jaehyungkim@kaist.ac.kr>
2023-06-12 17:26:02 +0900

    0612

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
-DATASET=qnli
+DATASET=cola
 SEED="1234"
-GPU=2
-EPOCHS=10
-RATIO=1.0
+GPU=1
+EPOCHS=3
 BATCH_SIZE=16
-BACKBONE=roberta_large
+BACKBONE=roberta_large # roberta_mc_large for winogrande, roberta_large for the others 
 
-# Step1
-for seed in $SEED
-do
-  CUDA_VISIBLE_DEVICES=$GPU python train.py --train_type 0611_base_noisy --noisy_label_criteria random --noisy_label_ratio 0.2 --data_ratio $RATIO --batch_size $BATCH_SIZE --epochs $EPOCHS --dataset $DATASET --seed $seed --backbone $BACKBONE
-done
+# Training of classifier 
+#for seed in $SEED
+#do
+#  CUDA_VISIBLE_DEVICES=$GPU python train.py --train_type xxxx_base --save_ckpt --batch_size $BATCH_SIZE --epochs $EPOCHS --dataset $DATASET --seed $seed --backbone $BACKBONE
+#  CUDA_VISIBLE_DEVICES=$GPU python train.py --grad_accumulation 4 --train_type xxxx_base --batch_size $BATCH_SIZE --epochs $EPOCHS --dataset $DATASET --seed $seed --backbone $BACKBONE
+#done
+
+#CUDA_VISIBLE_DEVICES=$GPU python construct_infoverse.py --train_type xxxx_base --seed_list $SEED --batch_size $BATCH_SIZE --epochs $EPOCHS --dataset $DATASET --seed 1234 --backbone $BACKBONE
